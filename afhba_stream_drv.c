098455ed448c09d4d8bf5da8f8a1c4ae83d62015
Peter Milne <peter.milne@d-tacq.com>
2015-09-01 16:34:02 +0100

    include kmcu in is_valid_z_ident() 	modified:   afhba_stream_drv.c

--- afhba_stream_drv.c
+++ afhba_stream_drv.c
@@ -361,6 +361,10 @@ static int is_valid_z_ident(unsigned z_ident, char buf[], int maxbuf)
 		snprintf(buf, maxbuf, "acq2106_%03d.comms%X",
 				z_ident&0x0ffff, (z_ident&0x00f00000)>>20);
 		return 1;
+	}else if ((z_ident&0xe4330000) == 0xe4330000){
+		snprintf(buf, maxbuf, "kmcu_%03d.comms%x",
+				z_ident&0x0ffff, (z_ident&0x00f00000)>>20);
+		return 1;
 	}else{
 		return 0;
 	}
